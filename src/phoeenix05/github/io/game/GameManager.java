c86d16f15b2fa935bc98f1aefeeabf4af73fa296
Anton Fredriksson <79942428+Phoeenix05@users.noreply.github.com>
2021-03-24 20:50:22 +0200

    Update

--- src/phoeenix05/github/io/game/GameManager.java
+++ src/phoeenix05/github/io/game/GameManager.java
@@ -10,10 +10,12 @@ import phoeenix05.github.io.gfx.ImageTile;
 
 public class GameManager extends AbstractGame {
 
-  private ImageTile image;
+  // private ImageTile tilemap;
+  private Image arrow;
 
   public GameManager() {
-    image = new ImageTile("/res/textures/tilemap.png", 16, 16);
+    // tilemap = new ImageTile("/res/textures/tilemap.png", 16, 16);
+    arrow = new Image("/res/textures/arrow.png");
   }
 
   @Override
@@ -27,7 +29,8 @@ public class GameManager extends AbstractGame {
 
   @Override
   public void render(GameContainer gc, Renderer r) {
-    r.drawImageTile(image, gc.getInput().getMouseX() - 8, gc.getInput().getMouseY() - 8, (int) temp, 0);
+    // r.drawImageTile(tilemap, gc.getInput().getMouseX() - 8, gc.getInput().getMouseY() - 8, (int) temp, 0);
+    r.drawImage(arrow, gc.getInput().getMouseX() - 8, gc.getInput().getMouseY() - 8);
   }
 
   public static void main(String[] args) {
