4ca2e89f72ebcc9814a28c33191f298ad7931167
Phoeenix05 <79942428+Phoeenix05@users.noreply.github.com>
2021-03-24 12:46:16 +0200

    Image
    Added ability to draw images

---
+++ README.md
@@ -0,0 +1,4 @@
+
+# Tutorial playlist link 
+# [https://www.youtube.com/watch?v=4iPEjFUZNsw&list=PL7dwpoQd3a8j6C9p5LqHzYFSkii6iWPZF]
+> Next episode is episode number 6
--- src/phoeenix05/github/io/game/GameManager.java
+++ src/phoeenix05/github/io/game/GameManager.java
@@ -5,23 +5,26 @@ import java.awt.event.KeyEvent;
 import phoeenix05.github.io.AbstractGame;
 import phoeenix05.github.io.GameContainer;
 import phoeenix05.github.io.Renderer;
+import phoeenix05.github.io.gfx.Image;
 
 public class GameManager extends AbstractGame {
 
-  public GameManager() {
+  private Image image;
 
+  public GameManager() {
+    image = new Image("/res/barrier.png");
   }
 
   @Override
   public void update(GameContainer gc, float dt) {
-    if (gc.getInput().isKeyDown(KeyEvent.VK_ESCAPE)) {
+    if (gc.getInput().isKeyDown(KeyEvent.VK_A)) {
       System.out.println("A was pressed");
     }
   }
 
   @Override
   public void render(GameContainer gc, Renderer r) {
-   
+    r.drawImage(image, gc.getInput().getMouseX(), gc.getInput().getMouseY());
   }
 
   public static void main(String[] args) {
