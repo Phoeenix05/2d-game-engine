256da4a70bfc7c35ddc43ebfb1fa23005327864e
Phoeenix05 <79942428+Phoeenix05@users.noreply.github.com>
2021-03-27 10:06:40 +0200

    Updated rendering code

--- src/phoeenix05/github/io/Renderer.java
+++ src/phoeenix05/github/io/Renderer.java
@@ -21,12 +21,12 @@ public class Renderer {
 
   public void clear() {
     for (int i = 0; i < p.length; i++) {
-      p[i] = 0;
+      p[i] = 0; // p[i] = Hex Value for other color
     }
   }
 
   public void setPixel(int x, int y, int value) {
-    if ((x < 0 || x >= pW || y < 0 || y >= pH) || value == 0xffff00ff) {
+    if ((x < 0 || x >= pW || y < 0 || y >= pH) || ((value >> 24) & 0xff) == 0) {
       return;
     }
 
@@ -58,8 +58,10 @@ public class Renderer {
     if (offX >= pW) return;
     if (offY >= pH) return;
     
-    int newX = 0, newY = 0;
-    int newWidth = image.getW(), newHeight = image.getH();
+    int newX = 0;
+    int newY = 0;
+    int newWidth = image.getW();
+    int newHeight = image.getH();
 
     if (offX < 0) {newX -= offX;}
     if (offY < 0) {newY -= offY;}
@@ -79,8 +81,10 @@ public class Renderer {
     if (offX >= pW) return;
     if (offY >= pH) return;
     
-    int newX = 0, newY = 0;
-    int newWidth = image.getTileW(), newHeight = image.getTileH();
+    int newX = 0;
+    int newY = 0;
+    int newWidth = image.getTileW(); 
+    int newHeight = image.getTileH();
 
     if (offX < 0) {newX -= offX;}
     if (offY < 0) {newY -= offY;}
@@ -91,6 +95,41 @@ public class Renderer {
       for (int x = newX; x < newWidth; x++) {
         setPixel(x + offX, y + offY, image.getP()[(x + tileX * image.getTileW()) + (y + tileY * image.getTileH()) * image.getW()]);
       } 
+    }
+  }
+
+  public void drawRect(int offX, int offY, int width, int height, int color) {
+    for (int y = 0; y <= height; y++) {
+      setPixel(offX, y + offY, color);
+      setPixel(offX + width, y + offY, color);
+    }
+
+    for (int x = 0; x <= width; x++) {
+      setPixel(x + offX, offY, color);
+      setPixel(x + offX, offY + height, color);
+    }
+  }
+
+  public void drawFillRect(int offX, int offY, int width, int height, int color) {
+    if (offX < -width / 4) return;
+    if (offY < -height / 4) return;
+    if (offX >= pW) return;
+    if (offY >= pH) return;
+    
+    int newX = 0;
+    int newY = 0;
+    int newWidth = width;
+    int newHeight = height;
+
+    if (offX < 0) {newX -= offX;}
+    if (offY < 0) {newY -= offY;}
+    if (newWidth + offX > pW) {newWidth -= (newWidth + offX - pW);}
+    if (newHeight + offY > pH) {newHeight -= (newHeight + offY - pH);}
+    
+    for (int y = newY; y <= newHeight; y++) {
+      for (int x = newX; x <= newWidth; x++) {
+        setPixel(x + offX, y + offY, color);
+      }
     }
   }
 
--- src/phoeenix05/github/io/game/GameManager.java
+++ src/phoeenix05/github/io/game/GameManager.java
@@ -5,32 +5,30 @@ import java.awt.event.KeyEvent;
 import phoeenix05.github.io.AbstractGame;
 import phoeenix05.github.io.GameContainer;
 import phoeenix05.github.io.Renderer;
+import phoeenix05.github.io.audio.SoundClip;
 import phoeenix05.github.io.gfx.Image;
 import phoeenix05.github.io.gfx.ImageTile;
 
 public class GameManager extends AbstractGame {
 
-  // private ImageTile tilemap;
-  private Image arrow;
-
   public GameManager() {
-    // tilemap = new ImageTile("/res/textures/tilemap.png", 16, 16);
-    arrow = new Image("/res/textures/arrow.png");
+    
   }
 
   @Override
   public void update(GameContainer gc, float dt) {
-    temp += dt * 4;
+    // Animation speed
+    // temp += dt * 5;
+    // if (temp > 4) temp = 0;
 
-    if (temp > 4) temp = 0;
+
   }
 
-  float temp = 0;
+  // float temp = 0;
 
   @Override
   public void render(GameContainer gc, Renderer r) {
-    // r.drawImageTile(tilemap, gc.getInput().getMouseX() - 8, gc.getInput().getMouseY() - 8, (int) temp, 0);
-    r.drawImage(arrow, gc.getInput().getMouseX() - 8, gc.getInput().getMouseY() - 8);
+    r.drawFillRect(gc.getInput().getMouseX() - 16, gc.getInput().getMouseY() - 16, 32, 32, 0xffffccff);
   }
 
   public static void main(String[] args) {
